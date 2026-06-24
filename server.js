1d7954e3bca82f4918c52d795fa658480b652d0c
Dekel122 <106159413+Dekel122@users.noreply.github.com>
2026-06-25 02:12:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/health', (req, res) => {
+    res.send('השרת חי ובועט!');
+});
+
+const express = require('express');
 const axios = require('axios');
 const app = express();
 
